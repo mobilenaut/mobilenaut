e670769ac0162cedb8e5cb0f24b3712aaee91b54
Jagan Ganesh <jagan@MacBook.local>
2025-02-14 13:08:29 +0530

    Base Version 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,158 @@
+/* IMPORTANT: Each array must have at least 1 element
+  and you must remove all elements within "#rotating-words"
+  and "#rotating-paragraphs".
+  You can also bypass the dynamic elements entirely
+  and uncomment the elements in index.html
+*/
+var loadDynamically = true;
+
+/* The beginning of the header sentence has a black font.
+  The headersBlack element of index i, will be added to
+  the corresponding headersBlue element of index i.
+  E.g. headersBlack[i] + headersBlue[i]
+  headersBlack[0] + headersBlue[0]
+  "Hi, I'm " + "Dan" = "Hi, I'm Dan"
+  
+  If one array is larger than the other, the last element
+  of the smaller array will be used for the rest of the
+  rotations. You can also use empty strings ""
+  E.g. headersBlack[1] doesn't exist
+  "Hi, I'm a student." 
+*/
+var headersBlack = [
+  //headersBlack + headersBlue, e.g. "Hi, I'm Dan"
+  "Hi, I'm ",
+];
+
+var headersBlue = [
+  //Descriptive and re-affirming words here
+  "Jagan Ganesh.",
+  "a Freelance Consultant.",
+  "a JavaScript Enthusiast.",
+  "a Software Engineer.",
+  "a Content Creator.",
+];
+
+/* If true, the paragraph beneath the header will also
+  rotate. If disabled, the first element of the array
+  will be used for the paragraph.
+  "rotating-paragraphs" div must also be of class
+  "slider"
+*/
+var rotateParagraph = false;
+
+/* Connects paragraph to header slider so they slide
+  at the same time.
+*/
+var connectParagraphToHeaders = false;
+
+/* The paragraphs to rotate between. If rotateParagraph is disabled,
+  the first element also serves as the only paragraph. You can
+  also use tags such as <a>, <span>, and a few more here.
+*/
+var rotatingParagraphs = [
+  "As a Lead Software Engineer with over 9 years of experience, I transform ideas into powerful, high-performance web and mobile applications. My specialization lies in creating scalable, user-friendly solutions that engage users and deliver tangible results for businesses.",
+];
+
+var addRotatingWords = function () {
+  var wordsDiv = document.getElementById("rotating-words");
+  var paragraphsDiv = document.getElementById("rotating-paragraphs");
+  var largestArrayLength;
+
+  if (!paragraphsDiv.classList.contains("slider")) {
+    paragraphsDiv.classList.add("slider");
+  }
+
+  if (rotateParagraph) {
+    largestArrayLength = Math.max(
+      headersBlack.length,
+      headersBlue.length,
+      rotatingParagraphs.length
+    );
+  } else {
+    largestArrayLength = Math.max(headersBlack.length, headersBlue.length);
+  }
+
+  for (var index = 0; index < largestArrayLength; index++) {
+    //Temp variables
+    var headerBeginning;
+    var headerEnd;
+    var paragraph = rotatingParagraphs[0]; //Default
+
+    if (index >= headersBlack.length) {
+      //Use last element
+      headerBeginning = headersBlack[headersBlack.length - 1];
+    } else {
+      headerBeginning = headersBlack[index];
+    }
+
+    if (index >= headersBlue.length) {
+      headerEnd = headersBlue[headersBlue.length - 1];
+    } else {
+      headerEnd = headersBlue[index];
+    }
+
+    if (index >= rotatingParagraphs.length) {
+      paragraph = rotatingParagraphs[rotatingParagraphs.length - 1];
+    } else {
+      paragraph = rotatingParagraphs[index];
+    }
+
+    //Add headers
+    var newDiv = document.createElement("div");
+    newDiv.className = "item";
+
+    var newElement = document.createElement("h4");
+    var textBeginning = document.createTextNode(headerBeginning);
+    var blueElement = document.createElement("a");
+    var textBlue = document.createTextNode(headerEnd);
+
+    newElement.appendChild(textBeginning);
+    blueElement.appendChild(textBlue);
+    blueElement.className = "highlighted-words"; //Used for dark theme
+    newElement.appendChild(blueElement);
+
+    newDiv.appendChild(newElement);
+    wordsDiv.appendChild(newDiv);
+
+    //Add paragraphs
+    var newPDiv = document.createElement("div");
+    newPDiv.className = "item";
+
+    var newPElement = document.createElement("p");
+    var textParagraph = document.createTextNode("");
+    newPElement.innerHTML = paragraph; //So you can use html tags inside of paragraph.
+
+    newPElement.appendChild(textParagraph);
+    if (connectParagraphToHeaders) {
+      newDiv.appendChild(newPElement);
+    } else {
+      newPDiv.appendChild(newPElement);
+      paragraphsDiv.appendChild(newPDiv);
+    }
+  }
+};
+
+var startTinySlider = function () {
+  var sliders = document.querySelectorAll(".slider");
+  sliders.forEach((slider) => {
+    var tnsSlider = tns({
+      container: slider,
+      mode: "gallery", //carousel is also nice
+      controls: false,
+      nav: false,
+      autoplay: true,
+      autoplayButtonOutput: false,
+      mouseDrag: true,
+    });
+  });
+  /*tns({
+    
+  });*/
+};
+
+//On start
+if (loadDynamically) {
+  addRotatingWords();
+}
+startTinySlider();
